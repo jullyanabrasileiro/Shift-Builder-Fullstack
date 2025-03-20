fa9109cddadb6dfc458f91ace308a09ac1a147eb
Jullyana <jullyana.brasileiro@hotmail.com>
2025-03-19 18:29:31 -0700

    worker model and service enviroment created

--- frontend/src/app/components/admin/all-shifts/all-shifts.component.ts
+++ frontend/src/app/components/admin/all-shifts/all-shifts.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShiftsService } from '../../../services/shifts.service';
+import { Shift } from '../../../models/shift.model';
 
 @Component({
   selector: 'app-all-shifts',
-  imports: [],
   templateUrl: './all-shifts.component.html',
-  styleUrl: './all-shifts.component.css'
+  styleUrls: ['./all-shifts.component.css'],
 })
-export class AllShiftsComponent {
+export class AllShiftsComponent implements OnInit {
+  shifts: Shift[] = [];
+  searchWorkerName: string = '';
+  searchPlace: string = '';
+  startDate: string = '';
+  endDate: string = '';
 
+  constructor(private shiftsService: ShiftsService) {}
+
+  ngOnInit(): void {
+    this.getAllShifts();
+  }
+
+  getAllShifts(): void {
+    this.shiftsService.getShifts().subscribe((data) => {
+      this.shifts = data;
+    });
+  }
+
+  filterShifts(): void {
+    this.shiftsService
+      .filterShifts(this.searchWorkerName, this.searchPlace, this.startDate, this.endDate)
+      .subscribe((data) => {
+        this.shifts = data;
+      });
+  }
+
+  editShift(id: number): void {
+    // Navegação para página de edição
+  }
 }
--- frontend/src/app/components/admin/all-workers/all-workers.component.ts
+++ frontend/src/app/components/admin/all-workers/all-workers.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkersService } from 'src/app/services/workers.service';
+import { Worker } from 'src/app/models/worker.model';
 
 @Component({
   selector: 'app-all-workers',
-  imports: [],
   templateUrl: './all-workers.component.html',
-  styleUrl: './all-workers.component.css'
+  styleUrls: ['./all-workers.component.css'],
 })
-export class AllWorkersComponent {
+export class AllWorkersComponent implements OnInit {
+  workers: Worker[] = [];
 
+  constructor(private workersService: WorkersService) {}
+
+  ngOnInit(): void {
+    this.getAllWorkers();
+  }
+
+  getAllWorkers(): void {
+    this.workersService.getWorkers().subscribe((data) => {
+      this.workers = data;
+    });
+  }
+
+  viewProfile(workerId: number): void {
+    // Navegação para perfil do worker
+  }
 }
